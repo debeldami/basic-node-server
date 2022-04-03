323d185aa5e3d85627f94d66f62db3b1f7826412
debeldami <debeldamilola@gmail.com>
2022-04-03 17:47:01 +0100

    friends and messages end points

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  if (req.url === '/friends') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(
+      JSON.stringify({
+        id: 123,
+        name: 'ku',
+      })
+    );
+  } else if (req.url === '/messages') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    res.write(`<html><body>
+    <ul>
+    <li>hi</li>
+    <li>hello</li>
+    </ul> 
+    </body></html>`);
+  }
+});
+
+server.listen(PORT, () => console.log(`listening at port ${PORT}`));
+//server.close();
